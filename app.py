0c6281a0e60eb27fe35d0617d9c03a2efac96b75
Derrick Lui <derricklui1@gmail.com>
2015-10-11 16:04:56 -0400

    updated app.py and utils.py, updated routes

--- app.py
+++ app.py
@@ -5,32 +5,41 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template(home.html)
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return render_template(about.html)
+    return render_template("about.html")
 
 
 
 @app.route("/login", methods = ["GET"])
 def login():
-    return render_template(login.html)
+    return render_template("login.html")
 
 @app.route("/login", methods = ["POST"])
 def login2():
     user = request.form['username']
     password = request.form['password']
     button = request.form['button']
-    if util.check(user,password):
+
+    if utils.authenticate(user,password):
         if user not in session:
             session[user] = True 
+   # else:
+   #     if user not in session:
+   #         session[user] = False
+   #         error = "invalid username or password"
+
+#add an Error message to the login page when it's incorrect
+
     return redirect(url_for("home"))
 
-@app.route("/logout")
-def logout():
-    
+#for basic purposes, user = "admin" and password = "password"
 
+@app.route("/reset")
+def reset():
+    session['user'] = False
     return redirect(url_for("home"))
 
 
