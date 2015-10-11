d52b102501d81b2cedb093393ef4629eff3f313c
az0977776 <az0977776@yahoo.com>
2015-10-11 01:11:34 -0400

    nothing much

--- app.py
+++ app.py
@@ -4,19 +4,34 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
+def home():
+    return render_template(home.html)
+
+@app.route("/about")
+def about():
+    return render_template(about.html)
 
 
 
+@app.route("/login", methods = ["GET"])
+def login():
+    return render_template(login.html)
 
+@app.route("/login", methods = ["POST"])
+def login2():
+    user = request.form['username']
+    password = request.form['password']
+    button = request.form['button']
+    if util.check(user,password):
+        if user not in session:
+            session[user] = True 
+    return redirect(url_for("home"))
 
+@app.route("/logout")
+def logout():
+    
 
-
-
-
-
-
-
-
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
