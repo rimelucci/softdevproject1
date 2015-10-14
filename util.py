4191f12b678a1f6508430d814a255b5097679e9c
Derrick Lui <derricklui1@gmail.com>
2015-10-13 21:00:08 -0400

    updated

--- util.py
+++ util.py
@@ -4,4 +4,11 @@ def authenticate(username,password):
     else:
         return False
 
+def makePost(username,title,body):
+    conn = sqlite3.connect("myDatabase.db")
+    c = conn.cursor()
+    ans = c.execute('insert into posts values("'+username+'","'+title'","'+body'");')
+    conn.commit()
+    return True;
+    #adds a post to the database based on parameters
     
