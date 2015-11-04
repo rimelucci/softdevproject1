5828c8c4e720dd770430dd212bcaac4dfee1f39e
M4598BM <milorobin@gmail.com>
2015-11-03 21:44:05 -0500

    replaced SQL with Mongo

--- util.py
+++ util.py
@@ -1,26 +1,35 @@
-import sqlite3
+import pymongo
 import hashlib
+
+#Mongo:
+
+#        You can use dictionaries like python
+
+#        Unlike python you can add and set with .
+
+#        db.<collection>.insert(x) inserts the dictionary x
+
+#        A list of dictionaries can be entered into a collection, which is really good
+
+#        db.<collection>.find() displays all
+
+#        db.<collection>.find({name : "  ", age : 30}) finds all with those aspects
+
+#        db.people.find({$or:[{name:"Dana"}, {age:{$gt:20}}]}) will look for anyone with that name or with age greater than 20
+
+#        db.people.update({name : "thomas"}, {$set:{age:50, last:"thompson"}}) finds anyone with name : "thomas" and sets their age to 50
+
+#        db.people.update({name:"Dana"}, {$inc:{age:5}})
+
+#        adding ,{upsert:true} before the last paren adds the person if they dont exist
+
 
 def initializeTables():
     #create the user table in user.db if the table does not already exist
-    conn = sqlite3.connect("databases/users.db")
-    c = conn.cursor()
-    c.execute('''
-    CREATE TABLE IF NOT EXISTS users
-    (username text, password text)
-    ''')
-    conn.commit()
-    conn.close()
+    db.createCollection("users", {})
 
     #create the posts table in myDatabase.db if the table does not already exist
-    conn = sqlite3.connect("databases/myDatabase.db")
-    c = conn.cursor()
-    c.execute('''
-    CREATE TABLE IF NOT EXISTS posts 
-    (username text, title text, body text)
-    ''')
-    conn.commit()
-    conn.close()
+    db.createCollection("posts", {})
 
 
 #authenticate checks the usernames and if the hash of the password matches with the password already in the database
@@ -32,9 +41,7 @@ def authenticate(username,password):
 #        return False
 
     hpass = hashlib.sha224(password).hexdigest()
-    conn = sqlite3.connect("databases/users.db")
-    c = conn.cursor()
-    ans = c.execute('SELECT * FROM users WHERE username = "'+username+'" and password = "'+hpass+'";')
+    ans = db.users.find({uname : username, pword : hpass})
     for r in ans:
         return True
     return False
@@ -42,9 +49,7 @@ def authenticate(username,password):
     #passwords stored as hashlib.sha224(<password>).hexdigest()
 
 def registerCheck(username):
-    conn = sqlite3.connect("databases/users.db")
-    c = conn.cursor()
-    ans = c.execute('SELECT * FROM users WHERE username = "'+username+'";')
+    ans = db.users.find({uname : username})
     for r in ans:
         return False
     return True
@@ -54,28 +59,19 @@ def registerCheck(username):
 def register(username,password):
     hpass = hashlib.sha224(password).hexdigest()
     
-    conn = sqlite3.connect("databases/users.db")
-    c = conn.cursor()
-    c.execute('INSERT INTO users VALUES("'+username+'","'+hpass+'");')
-    conn.commit()
-    conn.close()
+    if (!registerCheck(username)):
+        db.users.insert({uname : username, pword : hpass})
     #check if username is in the database already
     #might put this check somewhere else?
     
 
 
 def makePost(username,title,body):
-    conn = sqlite3.connect("databases/myDatabase.db")
-    c = conn.cursor()
-    ans = c.execute('INSERT INTO posts VALUES("'+username+'","'+title+'","'+body+'");')
-    conn.commit()
+    db.posts.insert({uname : username, title : title, body : body})
     #adds a post to the database based on parameters
 
 def getAllPosts():
-    conn = sqlite3.connect("databases/myDatabase.db")
-    c = conn.cursor()
-    c.execute('select * from posts;')
-    return c.fetchall();
+    return db.posts.find()
     #2d array 
     #first index = row id
     #second index - 0=name, 1=title, 2=body
